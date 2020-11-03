a5d0b184ca443369f7f491587b59df9fd93a500d
Boldeusz <mnpower@poczta.onet.pl>
2020-11-03 16:04:52 +0100

    update

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,14 @@ public class Hello {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello World");
+
+        int myFirstNumber = 5;
+
+        System.out.println(myFirstNumber);
+
+        double numberOfPounds = 200d;
+        double convertedKilograms = numberOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms= " + convertedKilograms);
     }
 }
